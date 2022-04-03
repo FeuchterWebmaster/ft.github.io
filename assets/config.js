b82ec405eb302d9b890022b6fbabd15263be482b
FeuchterWebmaster <feuchtetube@protonmail.com>
2022-04-03 16:49:01 +0200

    meta data

--- assets/config.js
+++ assets/config.js
@@ -5,8 +5,8 @@ const config = {
     meta: {
         htmlLang: "de",
         title: "Feuchte Tube - dein Sender für kostenlose Pornos",
-        description: "Dein kostenloser feuchter Porno-Sender. Feuchte Girls rund um die Uhr. Schalte gratis inund finde dein tabuloses feuchtes Mädchen. Amateur Porno aus Deutschland.",
-        keywords: "tube, youporn, pornhub, cliphunter, red tube, xxx, porn, xvideos",
+        description: "Kostenlose deutsche Pornos auf Feuchte Tube. Deutschlands's bekannteste Tube Seite ist zurück. Amateur Pornos direkt aus deutschen Schlafzimmern.",
+        keywords: "feuchte tube, pornos, deutsch, amateur, xxx, porn, german",
     },
     tags: {
         includeTags: ["german"],
@@ -16,63 +16,63 @@ const config = {
         limit: 80
     },
     categories: [
-        { name: 'Abspritzen', tag: 'cumshots', title: 'Abspritzen Pornos', image: 'abspritzen.jpg', publish: false, metaDescription: 'Deutsche Frauen schlucken in diesen geilen Tube Videos. Geile Abspritz Pornos zum konstenlosen anschauen ohne Anmeldung.' },
+        { name: 'Abspritzen', tag: 'cumshots', title: 'Abspritzen Pornos', image: 'abspritzen.jpg', publish: false, metaDescription: 'Deutsche Frauen schlucken in diesen geilen Tube Videos. Geile Abspritz Pornos zum kostenlosen anschauen ohne Anmeldung.' },
 
-        { name: 'Alt fickt Jung', tag: 'oldyoung', title: 'Alt fickt Jung Pornos', image: 'alt-fickt-jung.jpg', publish: true, metaDescription: 'In diesen deutschen Pornos siehst du Alt und Jung beim Ficken. Alte Opas ficken junge Mädchen und junge Männer bumsen alte Omas. Gratis tabulose Pornos mit Altersunterschied.' },
+        { name: 'Alt fickt Jung', tag: 'oldyoung', title: 'Alt fickt Jung Pornos', image: 'alt-fickt-jung.jpg', publish: true, metaDescription: 'In diesen deutschen Pornos siehst du Alt und Jung beim Ficken. Alte Opas ficken junge Teens und junge Männer bumsen alte Omas. Pornos mit Altersunterschied.' },
 
         { name: 'Amateur', tag: 'amateur', title: 'Amateur Pornos', image: 'amateure.jpg', publish: true, metaDescription: 'Feuchte Amateur Mädchen zeigen dir alles in diesen kostenlosen Pornos. Tabulose Girls rund um die Uhr. Dein gratis Porno-Sender.' },
 
-        { name: 'Arschficken', tag: 'anal', title: 'Arschfick Pornos', image: 'arschficken.jpg', publish: true, metaDescription: 'Geile deutsche Arschfick Tube Videos zum kostenlosen anschauen. Täglich neue Videos. Brandaktuelle deutsche Pornofilme.' },
+        { name: 'Arschficken', tag: 'anal', title: 'Arschfick Pornos', image: 'arschficken.jpg', publish: true, metaDescription: 'Geile deutsche Arschfick Pornos zum kostenlosen anschauen. Täglich neue Anal Pornos. Brandaktuelle deutsche Analpornos.' },
 
-        { name: 'Blasen', tag: 'blowjob', title: 'Blasen Pornos', image: 'blasen.jpg', publish: true, metaDescription: 'Deutsche Mädchen beim Blasen. Geile Pornofilme kostenlos anschauen. Deutsche Tube Schlampen beim Blowjob, täglich neue Videos.' },
+        { name: 'Blasen', tag: 'blowjob', title: 'Blasen Pornos', image: 'blasen.jpg', publish: true, metaDescription: 'Deutsche Mädchen beim Blasen. Geile Pornos kostenlos anschauen. Deutsche Tube Schlampen beim Blowjob, täglich neue Videos.' },
 
-        { name: 'Blondinen', tag: 'blonde', title: 'Blondinen Pornos', image: 'blondinen.jpg', publish: true, metaDescription: 'Deutsche Pornofilme mit Blondinen. Kostenlos jetzt sofort anschauen ohne lästige Anmeldung. Tube Blondinen beim geilen Porno.' },
+        { name: 'Blondinen', tag: 'blonde', title: 'Blondinen Pornos', image: 'blondinen.jpg', publish: true, metaDescription: 'Deutsche Pornos mit Blondinen. Kostenlos jetzt sofort anschauen ohne lästige Anmeldung. Tube Blondinen beim geilen Porno.' },
 
-        { name: 'Brünetten', tag: 'brunette', title: 'Brünetten Pornos', image: 'bruenetten.jpg', publish: true, metaDescription: 'Geile Tube Pornos mit Brünetten zum kostenlosen anschauen ohne Anmeldung. Deutsche Brünetten in geilen Pornofilmen vollkommen gratis.' },
+        { name: 'Brünetten', tag: 'brunette', title: 'Brünetten Pornos', image: 'bruenetten.jpg', publish: true, metaDescription: 'Geile Tube Pornos mit Brünetten zum kostenlosen anschauen ohne Anmeldung. Deutsche Brünetten in geilen Pornos, vollkommen gratis.' },
 
-        { name: 'Dildo', tag: 'toys', title: 'Dildo Pornos', image: 'dildo.jpg', publish: true, metaDescription: 'Geile Deutsche Dildo Pornofilme. Diese deutschen Müttern und Teens lieben die Spiele mit geilen Dildos. Hier kannst du jeden Tag neue gratis Pornoclips anschauen.' },
+        { name: 'Dildo', tag: 'toys', title: 'Dildo Pornos', image: 'dildo.jpg', publish: true, metaDescription: 'Geile Deutsche Dildo Pornos. Diese deutschen Müttern und Teens lieben die Spiele mit geilen Dildos. Hier kannst du jeden Tag neue gratis Pornoclips anschauen.' },
 
-        { name: 'Dreier', tag: 'threesome', title: 'Dreier Pornos', image: 'dreier.jpg', publish: true, metaDescription: 'Geile deutsche Frauen beim Dreier Sex. German Tube Pornofilme mit Gruppensex. Gratis Pornos jeden Tag aufs neue, absolut Deutsch. Sexfilme ohne langdauernde Anmeldung.' },
+        { name: 'Dreier', tag: 'threesome', title: 'Dreier Pornos', image: 'dreier.jpg', publish: true, metaDescription: 'Geile deutsche Amateure beim Dreier Porno. Auf Feuchte Tube kannst du täglich neue Dreiersex Pornos anschauen, kostenlos und ohne Anmeldung.' },
 
-        { name: 'Fetisch', tag: 'fetish', title: 'Fetisch Pornos', image: 'fetisch.jpg', publish: true, metaDescription: 'Deutsche Fetisch Pornos. Feuchte Tube, der geile Sender für deutsche Pornos zum gratis anschauen. Jeden Tag neue Sexvideos.' },
+        { name: 'Fetisch', tag: 'fetish', title: 'Fetisch Pornos', image: 'fetisch.jpg', publish: true, metaDescription: 'Deutsche Fetisch Pornos. Feuchte Tube, der geile Sender für deutsche Pornos zum gratis anschauen. Jeden Tag neue Fetisch Pornos.' },
 
-        { name: 'Hardcore', tag: 'hardcore', title: 'Hardcore Pornos', image: 'hardcore.jpg', publish: true, metaDescription: 'Deutsche Hardcore Pornos gratis anschauen. Geile Deutsche Tube Nutten stehen auf Hardcore Sex und werden in diesen Pornofilmen hart gebumst. Alles absolut gratis.' },
+        { name: 'Hardcore', tag: 'hardcore', title: 'Hardcore Pornos', image: 'hardcore.jpg', publish: true, metaDescription: 'Deutsche Hardcore Pornos gratis anschauen. Geile Deutsche Tube Nutten stehen auf Hardcore Sex und werden in diesen Pornos hart gebumst. Alles absolut gratis.' },
 
         { name: 'Familie', tag: 'family', title: 'Familien Pornos', image: 'familie.jpg', publish: false, metaDescription: 'Inzest Porno bei denen die Mutter den Sohn fickt oder der Bruder die Schwester. Gratis Inzest Filme auf Feuchte Tube.' },
 
-        { name: 'Lesben', tag: 'lesbian', title: 'Lesben Pornos', image: 'lesben.jpg', publish: true, metaDescription: 'Geile deutsche Lesben Tube Pornos zum kostenlosen anschauen. Brandaktuelle deutsche Gratis Pornofilme umsonst.' },
+        { name: 'Lesben', tag: 'lesbian', title: 'Lesben Pornos', image: 'lesben.jpg', publish: true, metaDescription: 'Geile deutsche Lesben Pornos zum kostenlosen anschauen auf Feuchte Tube. Jeden Tag neue Lesben Pornos mit deutschen Amateuren.' },
 
-        { name: 'Mollige Frauen', tag: 'bbw', title: 'Mollige Frauen Pornos', image: 'mollige-frauen.jpg', publish: true, metaDescription: 'Geile mollige Frauen ficken in diesen geilen Tube Pornos. Du stehst auf dicke Frauen die gefickt werden wollen dann schalte jetzt rein und gucken gratis Pornofilme mit molligen Frauen.' },
+        { name: 'Mollige Frauen', tag: 'bbw', title: 'Mollige Frauen Pornos', image: 'mollige-frauen.jpg', publish: true, metaDescription: 'Geile mollige Frauen ficken in diesen geilen Tube Pornos. Du stehst auf dicke Frauen die gefickt werden wollen dann schalte jetzt rein.' },
 
-        { name: 'Mütter', tag: 'milf', title: 'Mutter Pornos', image: 'muetter.jpg', publish: true, metaDescription: 'Deutsche Tube Pornovideos mit deutschen Müttern die gefickt und gebumst werden. Schaue dir geile gratis Pornos ohne Anmeldung an bei denen die Mütter hart blasen und ficken.' },
+        { name: 'Mütter', tag: 'milf', title: 'Mutter Pornos', image: 'muetter.jpg', publish: true, metaDescription: 'Deutsche Mütter in Pornos am Ficken. Deutschen Mutter die gefickt und gebumst wird. Du findest auf Feuchte Tube jeden Tag neue Pornos mit deutschen Milfs.' },
 
-        { name: 'Natursekt', tag: 'pissing', title: 'Natursekt Pornos', image: 'natursekt.jpg', publish: true, metaDescription: 'Pornos für Natursekt Liebhaber, in denen sich Männer und Frauen gegenseitig anpinkeln und anpissen. Hier wird sich gegenseitig nass gemacht. Natursektvideos bei denen du als Urin-Fetischist garantiert zum Orgasmus kommst.' },
+        { name: 'Natursekt', tag: 'pissing', title: 'Natursekt Pornos', image: 'natursekt.jpg', publish: true, metaDescription: 'Pornos für Natursekt Liebhaber, in denen sich Männer und Frauen gegenseitig anpinkeln. Hier wird sich gegenseitig nass gemacht. Deutsche Natursekt Pornos.' },
 
-        { name: 'Öffentlichkeit', tag: 'public', title: 'Pornos in der Öffentlichkeit', image: 'oeffentlichkeit.jpg', publish: true, metaDescription: 'In aller Öffentlichkeit drehen hier junge Amateure Porno Videos. Ob im Zug oder Parkplatzsex an der Autobahn, kein Rastplatz wird ausgelassen um geil rumzuficken. Jeder öffentliche Ort wird genutzt um draußen Sex zu haben. Sexfilme aus dem Badezimmer, Toilette, Wald, Restaurant oder Bus und Bahn.' },
+        { name: 'Öffentlichkeit', tag: 'public', title: 'Pornos in der Öffentlichkeit', image: 'oeffentlichkeit.jpg', publish: true, metaDescription: 'Pornos in der Öffentlichkeit. Egal ob in der Bahn, beim Parkplatzsex, im Zug oder auf dem Rastplatz, in diesen deutschen Pornos wird öffentlich gefickt.' },
 
-        { name: 'Omasex', tag: 'granny', title: 'Omasex Pornos', image: 'oma.jpg', publish: true, metaDescription: 'Hier gibt es geile Oma Sex Pornos bei denen sich die alten Frauen mit dicken Titten von jungen Männern ficken lassen. Alte Senioren die geilen Sex haben wollen. Pornofilme die dir zeigen das Alt immer noch am besten ist."' },
+        { name: 'Omasex', tag: 'granny', title: 'Omasex Pornos', image: 'oma.jpg', publish: true, metaDescription: 'Hier gibt es geile Omasex Pornos bei denen sich die alten Omas mit dicken Titten von jungen Männern ficken lassen. Alte Senioren die geilen Sex haben wollen."' },
 
-        { name: 'Gruppensex', tag: 'groupsex', title: 'Gruppensex Pornos', image: 'gruppensex.jpg', publish: true, metaDescription: 'Deutsche Orgie Tube Videos zum sofortigen anschauen ohne Anmeldung. Sexvideos mit mehren Männern und Frauen. Jeden Tag neue gratis Orgie Pornos.' },
+        { name: 'Gruppensex', tag: 'groupsex', title: 'Gruppensex Pornos', image: 'gruppensex.jpg', publish: true, metaDescription: 'Deutsche Gruppensex und Orgien Pornos zum sofortigen anschauen ohne Anmeldung auf Feuchte Tube. Sexvideos mit mehren Männern und Frauen.' },
 
-        { name: 'Pornostars', tag: 'pornstar', title: 'Pornos mit Pornostars', image: 'pornostars.jpg', publish: true, metaDescription: 'Deutsche Pornostars Tube Pornos zum kostenlosen anschauen. Keine lästige Anmeldung. Sofort gratis anschauen. Gina Wild Pornos for free.' },
+        { name: 'Pornostars', tag: 'pornstar', title: 'Pornos mit Pornostars', image: 'pornostars.jpg', publish: true, metaDescription: 'Deutsche Pornostars in Tube Pornos zum kostenlosen anschauen. Keine lästige Anmeldung. Sofort gratis anschauen. Von Gina Wild bis Vivian Schmidt.' },
 
-        { name: 'Retro', tag: 'retro', title: 'Retro Pornos', image: 'retro.jpg', publish: true, metaDescription: 'Alte Retro Pornos aus den 70er und 80er Jahren mit Porno Stars wie John Holmes oder Linda Lovelace. Vintage Sex Videos aus den Anfangszeiten der Pornographie. Diese Klassiker haben das Pornogeschäft groß gemacht. Gratis anschauen.' },
+        { name: 'Retro', tag: 'retro', title: 'Retro Pornos', image: 'retro.jpg', publish: true, metaDescription: 'Alte Retro Pornos aus den 70er und 80er Jahren mit Porno Stars wie John Holmes oder Linda Lovelace. Vintage Porn aus den Anfangszeiten der Pornographie.' },
 
-        { name: 'Rothaarige', tag: 'redhead', title: 'Rothaarige Pornos', image: 'rothaarige.jpg', publish: true, metaDescription: 'Geile Rothaarige Mädchen mit bleicher Haut ficken in Amateur Pornos. Schaue kostenlos Pornofilme in denen junge Schlampen mit roten Haaren Sex haben. Red Head Bitches beim blasen gefilmt.' },
+        { name: 'Rothaarige', tag: 'redhead', title: 'Rothaarige Pornos', image: 'rothaarige.jpg', publish: true, metaDescription: 'Geile Rothaarige Mädchen mit bleicher Haut ficken in Amateur Pornos. Schaue Redhead Pornos in denen junge Schlampen mit roten Haaren Sex haben.' },
 
-        { name: 'Runterholen', tag: 'handjob', title: 'Runterholen Pornos', image: 'runterholen.jpg', publish: true, metaDescription: 'Geile deutsche Pornovideos bei denen geile Mädchen und Frauen den Männern einen runterholen. Kostenlos anschauen ohne lästige Anmeldung und täglich aktualisierte Pornos.' },
+        { name: 'Runterholen', tag: 'handjob', title: 'Runterholen Pornos', image: 'runterholen.jpg', publish: true, metaDescription: 'Geile deutsche Pornos bei denen geile Mädchen und Frauen den Männern einen runterholen. Kostenlos, ohne Anmeldung und täglich aktualisierte Handjob Pornos.' },
 
-        { name: 'Schwarze', tag: 'ebony', title: 'Schwarzen Pornos', image: 'schwarze.jpg', publish: true, metaDescription: 'Riesige Schwarze Schwänze beim Sex mit jungen, weißen Mädchen. Afrikanische Schönheiten ficken mit weißen Männern. Sehe hier ebony Pornos mit schwarzen Schönheiten und riesig geilen ghetto Ärschen.' },
+        { name: 'Schwarze', tag: 'ebony', title: 'Schwarzen Pornos', image: 'schwarze.jpg', publish: true, metaDescription: 'Riesige Schwarze Schwänze beim Sex mit weißen Mädchen. Afrikanische Schönheiten ficken mit weißen Männern. Sehe hier Ebony Pornos mit dicken Ghetto Ärschen.' },
 
-        { name: 'Selbstbefriedigung', tag: 'masturbation', title: 'Selbstbefriedigung Pornos', image: 'selbstbefriedigung.jpg', publish: true, metaDescription: 'Geile Deutsche Tube Pornos mit Teens und deutschen Müttern die sich selbstbefriedigen. Eine Orgasmus Garantie bei diesen geilen Sex Pornos.' },
+        { name: 'Selbstbefriedigung', tag: 'masturbation', title: 'Selbstbefriedigung Pornos', image: 'selbstbefriedigung.jpg', publish: true, metaDescription: 'Geile Deutsche Tube Pornos mit Teens und deutschen Müttern die sich selbst befriedigen. Eine Orgasmus Garantie bei diesen geilen Masturbations-Pornos.' },
 
-        { name: 'Strippen', tag: 'striptease', title: 'Strip Pornos', image: 'strippen.jpg', publish: true, metaDescription: 'Geile deutsche Teens und Mütter beim Strippen. Hier findest du geile deutsche Porn Tube Videos zum sofortigen anschauen ohne Anmeldung. Jeden Tag neue gratis Strip Clips.' },
+        { name: 'Strippen', tag: 'striptease', title: 'Strip Pornos', image: 'strippen.jpg', publish: true, metaDescription: 'Geile deutsche Teens und Mütter beim Strippen. Hier findest du geile deutsche Striptease und Lapdance Pornos zum sofortigen anschauen ohne Anmeldung.' },
 
-        { name: 'Jung', tag: 'teen', title: 'Pornos mit 18 jährigen', image: 'teens.jpg', publish: true, metaDescription: 'Junge Mädchen zeigen dir alles in diesen geilen Tube Pornos. Geile Amateur Girls und Mädchen ficken in diesen geilen Pornofilmen bis zum Orgasmus.' },
+        { name: 'Jung', tag: 'teen', title: 'Pornos mit 18 jährigen', image: 'teens.jpg', publish: true, metaDescription: 'Junge Teen Mädchen die gerade erst 18 Jahre alt geworden sind zeigen dir alles in diesen geilen Tube Pornos. Geile Amateur Girls, Teens und Mädchen Pornos.' },
 
-        { name: 'Titten', tag: 'big-tits', title: 'Titten Pornos', image: 'titten.jpg', publish: true, metaDescription: 'Kostenlose Deutsche Titten Pornos gratis anschauen ohne Anmeldung. Absolut gratis und täglich für euch aktualisiert. Dicke Titten, kleine Titten, Milch und hänge Titten.' },
+        { name: 'Titten', tag: 'big-tits', title: 'Dicke Titten Pornos', image: 'titten.jpg', publish: true, metaDescription: 'Deutsche Titten Pornos gratis anschauen ohne Anmeldung. Dicke Titten, kleine Titten, Milch und hänge Titten. Absolut gratis und täglich für euch aktualisiert.' },
 
-        { name: 'Zeichentrick', tag: 'anime', title: 'Zeichentrick Pornos', image: 'zeichentrick.jpg', publish: false, metaDescription: 'Anime, Hentai und Zeichentrick Pornos. Sex Animationen in 3d. Mangas in denen der Fantasie keine Grenzen gesetzt sind. Gratis 3d Comic Porn täglich aktualisiert.' },
+        { name: 'Zeichentrick', tag: 'anime', title: 'Zeichentrick Pornos', image: 'zeichentrick.jpg', publish: false, metaDescription: 'Anime und Zeichentrick Pornos. Sex Animationen in 3d. Mangas in denen der Fantasie keine Grenzen gesetzt sind. Gratis 3d Comic Porn täglich aktualisiert.' },
 
         { name: 'Hentai', tag: 'hentai', title: 'Hentai Pornos', image: 'hentai.jpg', publish: false, metaDescription: 'Schaue sofort Hentai und Anime Porno Filme als gratis Stream. Garantiert ohne blöde Anmeldung. Manga Porno und das Ganze auf deutsch.' }
     ]
